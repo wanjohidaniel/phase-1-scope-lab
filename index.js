427aab452b1fa6c6dead5dde487956490ab8350e
Daniel Wanjohi <danirunguwanjohi@gmail.com>
2024-03-23 09:50:00 -0700

    I am unable to complete it

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+function customerName(){
+    const myVar = 'bob';
+    return myVar;
+}
+
+function myVar(){
+    return upperCaseCustomerN;
+}
+
+function bestCustomer(){
+    const firstVar = 'not bob';
+    return firstVar;
+}
+firstVar;
+
+function overwriteBestCustomer(){
+    return bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = name;
+function changeLeastFavoriteCustomer(){
+    console.log(name);
+}
